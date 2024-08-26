c38d17d7e77b23f74c495471ba7806318e6408c0
iamxettry <rajuchhetry11@gmail.com>
2024-08-26 18:16:53 +0545

    feat : login url

--- apps/auth/urls.py
+++ apps/auth/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('register/user/', RegisterUserView.as_view(), name="Register-user" )
+    path('register/user/', RegisterUserView.as_view(), name="Register-user" ),
+    path('login/user/', LoginUserView.as_view(), name="Login-user" ),
 ]
