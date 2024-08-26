d873a762d2a02efefe9677f0e475dd69a5e74c2b
iamxettry <rajuchhetry11@gmail.com>
2024-08-26 09:11:41 +0545

    user model created

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager, PermissionsMixin
+from django.utils import timezone
+import uuid
+class UserManager(BaseUserManager):
+    def create_user(self,username,email,password=None,**extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        if not username:
+            raise ValueError("The user Field must be set")
+        
+        email=self.normalize_email(email)
+        user=self.model(
+            email=email,
+            username=username,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email,username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
-# Create your models here.
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(username=username, email=email, password=password,**extra_fields)
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30,unique=True)
+    profile_picture=models.ImageField(blank=True,null=True , upload_to='profile/', height_field=None, width_field=None, max_length=None)
+
+    otp = models.CharField(max_length=6, blank=True, null=True)
+    otp_created_at = models.DateTimeField(blank=True, null=True)
+    otp_tries = models.PositiveIntegerField(default=0)
+    email_verified = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+        
+    objects=UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
