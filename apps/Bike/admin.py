1ac77d745d0b029f09118e0b8c0d45169808b739
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-17 21:50:01 +0545

    feat : register admin bike model

--- apps/Bike/admin.py
+++ apps/Bike/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .views import *
+
+class AdminBike(admin.ModelAdmin):
+    list_display = ['brand', 'model', 'year', 'color', 'start', 'price', 'description', 'image', 'date']
+    search_fields = ['brand', 'model', 'year', 'color', 'start', 'price', 'description', 'image', 'date']
+    list_filter = ['brand', 'model', 'year', 'color', 'start', 'price', 'description', 'image', 'date']
+
+admin.site.register(Bike, AdminBike)
+
