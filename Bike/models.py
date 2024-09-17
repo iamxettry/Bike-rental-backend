0b0eb4b6051c6728683e950a633d87bc7251fb99
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-17 21:01:22 +0545

    feat : bike model added

--- Bike/models.py
+++ Bike/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import uuid
 
+
+START_CHOICES = (
+            ('SELF', 'SELF START ONLY'),
+            ('KICK_SELF', 'KICK & SELF START'),
+            ('KICK', 'KICK START ONLY'),
+        )
 # Create your models here.
+class Bike(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    color = models.CharField(max_length=100)
+    start = models.CharField(max_length=100, choices=START_CHOICES)
+    price = models.FloatField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/bikes')
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.brand + " " + self.model
