a0cd64332d4c1bfaf2b112d2e14456d69d583109
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-17 21:50:34 +0545

    feat : bike urls and views

--- apps/Bike/urls.py
+++ apps/Bike/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from .views import *
 
 urlpatterns = [
-
+    path('create/', BikeCreateView.as_view(), name='create'),
 ]
 
--- apps/Bike/views.py
+++ apps/Bike/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from .serializers import BikeSerializer
+from .models import Bike
 
-# Create your views here.
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
+
+# Bike Create API
+class BikeCreateView(CreateAPIView):
+    serializer_class = BikeSerializer
+    permission_classes = [IsAdminUser, IsAuthenticated]
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
