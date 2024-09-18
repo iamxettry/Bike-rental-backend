fc2498de39c5d322843aee54faf0e4902cf204f4
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-18 21:24:17 +0545

    validate price field

--- apps/Bike/serializers.py
+++ apps/Bike/serializers.py
@@ -1,9 +1,13 @@
 
-from rest_framework import serializers
-
+from rest_framework import serializers, exceptions
 from .models import Bike
 
 class BikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bike
         fields = '__all__'
+
+    def validate(self, attrs):
+        if attrs['price'] < 100:
+            raise exceptions.APIException("Price should be greater than 100.")
+        return super().validate(attrs)
