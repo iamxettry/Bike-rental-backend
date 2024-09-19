05e092e00f60de3b506540cd0c092950843da950
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-19 07:48:44 +0545

    feat : id field added

--- apps/Bike/admin.py
+++ apps/Bike/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from .views import *
 
 class AdminBike(admin.ModelAdmin):
-    list_display = ['brand', 'model', 'year', 'color', 'start', 'price', 'description', 'image', 'date']
-    search_fields = ['brand', 'model', 'year', 'color', 'start', 'price', 'description', 'image', 'date']
-    list_filter = ['brand', 'model', 'year', 'color', 'start', 'price', 'description', 'image', 'date']
+    list_display = ['id','brand', 'model', 'year', 'color', 'start', 'price', 'description', 'image', 'date']
+    search_fields = ['id','brand', 'model', 'year', 'color', 'start', 'price', 'description', 'image', 'date']
+    list_filter = ['id','brand', 'model', 'year', 'color', 'start', 'price', 'description', 'image', 'date']
 
 admin.site.register(Bike, AdminBike)
 
