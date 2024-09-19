9f4ce161ee268d16c20e2e2a164e671291dd8e84
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-19 07:28:45 +0545

    fix : response message

--- apps/Bike/views.py
+++ apps/Bike/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from .serializers import BikeSerializer
 from .models import Bike
+from rest_framework.response import Response
+from rest_framework import status
 
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
 
@@ -10,4 +12,8 @@ class BikeCreateView(CreateAPIView):
     serializer_class = BikeSerializer
     permission_classes = [IsAdminUser, IsAuthenticated]
     def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"success": "Bike Added Successfully."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
