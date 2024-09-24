6c34778a05d6e1db3cbb7855c857b8c3a722d7ca
Raju Chhetri <rajuchhetry11@gmail.com>
2024-09-24 20:50:28 +0545

    fix : bike features

--- apps/Bike/serializers.py
+++ apps/Bike/serializers.py
@@ -18,8 +18,23 @@ class BikeSerializer(serializers.ModelSerializer):
             raise exceptions.APIException("Price should be greater than 100.")
         return super().validate(attrs)
     
+    def create(self, validated_data):
+        # Extract features data
+        features_data = validated_data.pop('features')
+        
+        # Create the features instance
+        features = Features.objects.create(**features_data)
+        
+        # Create the bike with the features instance
+        bike = Bike.objects.create(features=features, **validated_data)
+        return bike
     def update(self, instance, validated_data):
 
+        features_data = validated_data.pop('features', None)
+        
+        if features_data:
+            # Update the features instance
+            Features.objects.filter(pk=instance.features.pk).update(**features_data)
         if validated_data.get('price') < 100:
             raise exceptions.APIException("Price should be greater than 100.")
 
