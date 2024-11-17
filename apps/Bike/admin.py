e8a98a3fb0ed1cd3bf486b07f55cd35b6bdf3768
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-17 22:23:15 +0545

    update admin register

--- apps/Bike/admin.py
+++ apps/Bike/admin.py
@@ -3,16 +3,16 @@ from django.contrib import admin
 from .models import *
 
 class AdminBike(admin.ModelAdmin):
-    list_display = ['id','brand', 'model', 'year', 'color', 'features', 'price', 'description', 'image', 'date']
-    search_fields = ['id','brand', 'model', 'year', 'color', 'features', 'price', 'description', 'image', 'date']
-    list_filter = ['id','brand', 'model', 'year', 'color', 'features', 'price', 'description', 'image', 'date']
+    list_display = ['id','brand', 'model', 'year', 'color', 'start','engine','distance', 'price', 'description', 'image', 'date']
+    search_fields = ['id','brand', 'model', 'year', 'color', 'start','engine','distance', 'price', 'description', 'image', 'date']
+    list_filter = ['id','brand', 'model', 'year', 'color', 'start','engine','distance', 'price', 'description', 'image', 'date']
 
 
-class AdminFeatures(admin.ModelAdmin):
-    list_display = ['start', 'engine', 'distance']
-    search_fields = ['start', 'engine', 'distance']
-    list_filter = ['start', 'engine', 'distance']
+# class AdminFeatures(admin.ModelAdmin):
+#     list_display = ['start', 'engine', 'distance']
+#     search_fields = ['start', 'engine', 'distance']
+#     list_filter = ['start', 'engine', 'distance']
 
 admin.site.register(Bike, AdminBike)
-admin.site.register(Features, AdminFeatures)
+# admin.site.register(Features, AdminFeatures)
 
