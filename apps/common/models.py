36be359844131caf4c6cf15a03afe0bf1885ec44
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-24 19:16:06 +0545

    add id in location model

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+import uuid
 # Create your models here.
 
 # Location Model
 class Location(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     city = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.city
