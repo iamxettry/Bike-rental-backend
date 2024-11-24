61f763c09b4ccc10464bdf8372c9cc97c4ee42dc
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-24 19:15:20 +0545

    feat: search location url

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('location-create/', LocationCreateView.as_view(), name='location-create'),
     path('location-list/', LocationListView.as_view(), name='location-create'),
     path('location-retrieve-update-delete/<uuid:pk>/', LocationRetriveUpdateDestroyView.as_view(), name='location-retrieve-update-delete'),
+    path('location-search/', LocationSearchView.as_view(), name='location-search'),
 ]
 
