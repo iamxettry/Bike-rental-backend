9e74f39d47d70196b7afbc2282440c0957e30ced
Raju Chhetri <rajuchhetry11@gmail.com>
2024-11-24 19:15:43 +0545

    admin register

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -4,4 +4,6 @@ from django.contrib import admin
 
 from .models import Location
 
-admin.site.register(Location)
+class AdminLocation(admin.ModelAdmin):
+    list_display = ['city']
+admin.site.register(Location, AdminLocation)
